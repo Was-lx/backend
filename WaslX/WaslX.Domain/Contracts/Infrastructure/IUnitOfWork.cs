d104c9eb4a5ff59625ed04101b203fdad8a28b7d
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-23 23:58:38 +0300

    Add Infrastructure Contract

---
+++ WaslX/WaslX.Domain/Contracts/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace WaslX.Domain.Contracts.Infrastructure;
+
+public interface IUnitOfWork : IAsyncDisposable
+{
+    Task<int> CompleteAsync();
+    IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+        where TEntity : class
+        where TKey : IEquatable<TKey>;
+}
