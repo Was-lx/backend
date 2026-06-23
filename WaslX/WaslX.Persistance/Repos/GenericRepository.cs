feb973b310c87523988e7ab8b4118350161141c4
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:01:03 +0300

    Add Generic Repo implamentation and SpecificationEvaluator

---
+++ WaslX/WaslX.Persistance/Repos/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WaslX.Domain.Contracts.Infrastructure;
+using WaslX.Domain.Contracts.Specification;
+using WaslX.Persistance.Data;
+
+namespace WaslX.Persistance.Repos;
+
+public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+where TEntity : class
+where TKey : IEquatable<TKey>
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<TEntity>();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false, CancellationToken cancellationToken = default)
+    {
+        return withTracking
+            ? await _dbSet.ToListAsync(cancellationToken)
+            : await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllWithSpecAsync(ISpecification<TEntity, TKey> specification, bool withTracking = false)
+    {
+        var query = withTracking ? _dbSet.AsQueryable() : _dbSet.AsNoTracking();
+        return await SpecificationEvaluator<TEntity, TKey>.GetQuery(query, specification).ToListAsync();
+    }
+
+    public async Task<int> GetCountWithSpecAsync(ISpecification<TEntity, TKey> specification, bool withTracking = false)
+    {
+        var query = withTracking ? _dbSet.AsQueryable() : _dbSet.AsNoTracking();
+        return await SpecificationEvaluator<TEntity, TKey>.GetQuery(query, specification).CountAsync();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default)
+        => await _dbSet.FindAsync([id], cancellationToken);
+
+    public async Task<TEntity?> GetWithSpecAsync(ISpecification<TEntity, TKey> specification)
+        => await SpecificationEvaluator<TEntity, TKey>.GetQuery(_dbSet.AsQueryable(), specification).FirstOrDefaultAsync();
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        => await _dbSet.AddAsync(entity, cancellationToken);
+
+    public void Update(TEntity entity)
+        => _dbSet.Update(entity);
+
+    public void Delete(TEntity entity)
+        => _dbSet.Remove(entity);
+}
---
+++ WaslX/WaslX.Persistance/Repos/SpecificationEvaluator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WaslX.Domain.Contracts.Specification;
+
+namespace WaslX.Persistance.Repos;
+
+internal static class SpecificationEvaluator<TEntity, TKey>
+where TEntity : class
+where TKey : IEquatable<TKey>
+{
+    public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> query, ISpecification<TEntity, TKey> specification)
+    {
+        if (specification.Criteria is not null)
+            query = query.Where(specification.Criteria);
+
+        if (specification.OrderByDesc is not null)
+            query = query.OrderByDescending(specification.OrderByDesc);
+        else if (specification.OrderBy is not null)
+            query = query.OrderBy(specification.OrderBy);
+
+        if (specification.Includes is not null)
+            query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+        if (specification.IsGettingTopQuery)
+            query = query.Take(specification.Take);
+        else if (specification.IsPaginationEnabled)
+            query = query.Skip(specification.Skip).Take(specification.Take);
+
+        return query;
+    }
+}
