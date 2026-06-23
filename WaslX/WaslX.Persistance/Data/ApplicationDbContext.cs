c487591fb925edb54f2bba5fe5c7bb2c4ac4af8b
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:00:15 +0300

    Add AppDbContext

---
+++ WaslX/WaslX.Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WaslX.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WaslX.Persistance.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
