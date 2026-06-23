c66ec80ca8e92b766e1f2a5ba24a46d14f2cb742
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:05:31 +0300

    Add IJwtTokenGenerator

---
+++ WaslX/WaslX.Application/Abstractions/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using WaslX.Application.Features.Auth.Dtos;
+
+namespace WaslX.Application.Abstractions.Authentication;
+
+public interface IJwtTokenGenerator
+{
+    AccessToken GenerateAccessToken(string userId, string email, string fullName, IEnumerable<string> roles);
+    RefreshTokenValue GenerateRefreshToken();
+}
