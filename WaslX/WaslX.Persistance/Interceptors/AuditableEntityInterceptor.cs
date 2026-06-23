92edc2c8b8257523fea70b2b686acb25ae63f6e8
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:00:32 +0300

    Add interceptor

---
+++ WaslX/WaslX.Persistance/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WaslX.Domain.Common;
+
+namespace WaslX.Persistance.Interceptors
+{
+    public class AuditableEntityInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is not null)
+                UpdateAuditFields(eventData.Context);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public override InterceptionResult<int> SavingChanges(
+            DbContextEventData eventData,
+            InterceptionResult<int> result)
+        {
+            if (eventData.Context is not null)
+                UpdateAuditFields(eventData.Context);
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        private static void UpdateAuditFields(DbContext context)
+        {
+            var entries = context.ChangeTracker
+                .Entries<BaseEntity>()
+                .Where(e => e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
