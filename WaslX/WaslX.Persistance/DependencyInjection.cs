03921b5653422c8a9d94edd94ab50f70c1b8c4e3
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:02:00 +0300

    Add Perestance DependencyInjection

---
+++ WaslX/WaslX.Persistance/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WaslX.Domain.Contracts.Infrastructure;
+using WaslX.Infrastructure.Identity;
+using WaslX.Persistance.Data;
+using WaslX.Persistance.Repos;
+using WaslX.Persistance.UnitOfWorks;
+
+namespace WaslX.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                .ConfigureWarnings(w =>
+                w.Ignore(RelationalEventId.PendingModelChangesWarning))
+                ;
+            });
+
+            services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
+
+            return services;
+        }
+
+    }
+}
