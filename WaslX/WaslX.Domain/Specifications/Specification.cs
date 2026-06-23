1979a9924a1a457305aaa5f6c14bdf46ff8cb26a
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-23 23:59:35 +0300

    Add Specification implementation

---
+++ WaslX/WaslX.Domain/Specifications/Specification.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using WaslX.Domain.Contracts.Specification;
+
+namespace WaslX.Domain.Specifications;
+
+public class Specification<TEntity, TKey> : ISpecification<TEntity, TKey>
+where TKey : IEquatable<TKey>
+{
+    public Expression<Func<TEntity, bool>>? Criteria { get; set; } = null;
+    public List<Expression<Func<TEntity, object>>> Includes { get; set; } = [];
+    public Expression<Func<TEntity, object>>? OrderBy { get; set; } = null;
+    public Expression<Func<TEntity, object>>? OrderByDesc { get; set; } = null;
+    public int Take { get; set; }
+    public int Skip { get; set; }
+    public bool IsPaginationEnabled { get; set; }
+    public bool IsGettingTopQuery { get; set; }
+
+    protected Specification() { }
+
+    protected Specification(Expression<Func<TEntity, bool>> expression)
+    {
+        Criteria = expression;
+    }
+
+    protected void AddInclude(Expression<Func<TEntity, object>> include)
+        => Includes.Add(include);
+
+    protected void AddOrderBy(Expression<Func<TEntity, object>> orderBy)
+        => OrderBy = orderBy;
+
+    protected void AddOrderByDesc(Expression<Func<TEntity, object>> orderByDesc)
+        => OrderByDesc = orderByDesc;
+
+    protected void ApplyPagination(int skip, int take)
+    {
+        IsPaginationEnabled = true;
+        Skip = skip;
+        Take = take;
+    }
+}
