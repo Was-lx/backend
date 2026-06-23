929e36cb81a7eebc3c1bbff704d8e0dcfe31e439
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:04:31 +0300

    Add Infrastructure Settings

---
+++ WaslX/WaslX.Infrastructure/Settings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WaslX.Infrastructure.Settings;
+
+public class AppSettings
+{
+    public const string SectionName = "App";
+
+    public string FrontendBaseUrl { get; set; } = string.Empty;
+}
---
+++ WaslX/WaslX.Infrastructure/Settings/HangfireSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WaslX.Infrastructure.Settings;
+
+public class HangfireSettings
+{
+    public const string SectionName = "Hangfire";
+
+    public int WorkerCount { get; set; } = 5;
+}
