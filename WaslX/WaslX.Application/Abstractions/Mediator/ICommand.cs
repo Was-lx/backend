45fe46c4c1f01e0042e2b6ed987c3b8247fc9944
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:05:57 +0300

    Add Application Abstractions

---
+++ WaslX/WaslX.Application/Abstractions/Mediator/ICommand.cs
@@ -0,0 +1,9 @@
+﻿
+using MediatR;
+using WaslX.Domain.Results;
+
+namespace WaslX.Application.Abstractions.Mediator;
+
+public interface ICommand : IRequest<Result>;
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>;
---
+++ WaslX/WaslX.Application/Abstractions/Mediator/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using WaslX.Domain.Results;
+
+namespace WaslX.Application.Abstractions.Mediator;
+
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand;
+
+public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>;
+
