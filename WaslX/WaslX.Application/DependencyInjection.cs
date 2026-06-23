7007d9060d2d9ae57cb6c10c627f29b95c059c65
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:06:35 +0300

    Add DI for Application

---
+++ WaslX/WaslX.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using WaslX.Application.Abstractions.Behaviors;
+
+namespace WaslX.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
+
+        var mapsterConfig = TypeAdapterConfig.GlobalSettings;
+        mapsterConfig.Scan(assembly);
+        services.AddSingleton(mapsterConfig);
+        services.AddScoped<IMapper, ServiceMapper>();
+
+        return services;
+    }
+}
