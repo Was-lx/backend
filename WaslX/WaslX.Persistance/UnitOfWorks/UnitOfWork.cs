11a3bf8f1757ecf690f99a05f599c94572bf56ad
Eng Mohamed yasser <mohamedyasserezz067@gmail.com>
2026-06-24 00:01:30 +0300

    Add UnitOfWork Implementation

---
+++ WaslX/WaslX.Persistance/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using WaslX.Domain.Contracts.Infrastructure;
+using WaslX.Persistance.Data;
+using WaslX.Persistance.Repos;
+
+namespace WaslX.Persistance.UnitOfWorks;
+
+public class UnitOfWork(ApplicationDbContext dbContext) : IUnitOfWork
+{
+    private readonly ConcurrentDictionary<string, object> _repositories = new();
+
+    public async Task<int> CompleteAsync() => await dbContext.SaveChangesAsync();
+
+    public ValueTask DisposeAsync() => dbContext.DisposeAsync();
+
+    public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+        where TEntity : class
+        where TKey : IEquatable<TKey>
+    {
+        return (IGenericRepository<TEntity, TKey>)_repositories
+            .GetOrAdd(typeof(TEntity).Name, new GenericRepository<TEntity, TKey>(dbContext));
+    }
+}
